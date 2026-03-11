0618e74e423e9809717a6c9790a33764059b3967
Dominatoxz <dominatomathus06@gmail.com>
2026-03-11 13:44:32 -0300

    11/03

--- models/Produto.php
+++ models/Produto.php
@@ -8,7 +8,9 @@ class Produto {
         return [ // => operador de atribuição de array
             ['nome' => 'Teclado Mecânico', 'preco' => 250],
             ['nome' => 'Mouse Gamer', 'preco' => 150],
-            ['nome' => 'Monitor 144hz', 'preco' => 1200]
+            ['nome' => 'Monitor 144hz', 'preco' => 1200],
+            ['nome' => 'Apple Iphone 23', 'preco' => 50000],
+            ['nome' => 'Processador Intel Celeron D310', 'preco' => 120]
         ];
     }
 }
